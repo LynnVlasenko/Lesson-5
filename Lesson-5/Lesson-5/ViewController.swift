d04572a332932f1e167ee90f519fd30a71593e4f
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-03-04 18:12:57 +0100

    Add empty project

---
+++ Lesson-5/Lesson-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-5
+//
+//  Created by Алина Власенко on 04.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
